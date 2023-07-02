7b6c218091f211a6a85d629385e144bf00a40c17
Kamatari-rin <kamatarisama@gmail.com>
2023-07-03 03:37:10 +0500

    ЭДобавил проверку нпри созданиÐ на Ðдубликат email

--- src/main/java/ru/practicum/shareit/user/repository/impl/UserRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/user/repository/impl/UserRepositoryImpl.java
@@ -1,47 +1,33 @@
 package ru.practicum.shareit.user.repository.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
-import ru.practicum.shareit.exception.AlreadyExistsException;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.util.*;
 
 @Repository
+@Slf4j
 public class UserRepositoryImpl implements UserRepository {
 
     private final Map<Long, User> userMap;
-    private final Set<String> emails;
     private Long id;
 
     public UserRepositoryImpl() {
         this.userMap = new HashMap<>();
-        this.emails = new HashSet<>();
         this.id = 1L;
     }
 
     @Override
     public Optional<User> create(User user) {
-        if (emails.contains(user.getEmail())) {
-            throw new AlreadyExistsException("Пользователь с таким Email уже существует.");
-        }
         user.setId(generatedNewId());
         userMap.put(user.getId(), user);
-        emails.add(user.getEmail());
         return getUserById(user.getId());
     }
 
     @Override
     public User update(Long userId, User user) {
-        final String userEmail = userMap.get(userId).getEmail();
-        final String updatedUserEmail = user.getEmail();
-
-        if (emails.contains(updatedUserEmail) && !userEmail.equals(updatedUserEmail)) {
-            throw new AlreadyExistsException("Пользователь с таким Email уже существует.");
-        }
-
-        emails.remove(userEmail);
-        emails.add(updatedUserEmail);
         userMap.put(userId, user);
         return getUserById(userId).get();
     }
@@ -58,7 +44,6 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public void delete(Long id) {
-        emails.remove(userMap.get(id).getEmail());
         userMap.remove(id);
     }
 
--- src/main/java/ru/practicum/shareit/user/service/impl/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.user.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.practicum.shareit.exception.AlreadyExistsException;
 import ru.practicum.shareit.exception.UserNotFoundException;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
@@ -10,6 +11,7 @@ import ru.practicum.shareit.user.repository.mapper.UserMapper;
 import ru.practicum.shareit.user.service.UserService;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -34,6 +36,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto create(UserDto userDto) {
+        Set<String> emails = getAllEmails();
+
+        if (emails.contains(userDto.getEmail())) {
+            throw new AlreadyExistsException("Пользователь с таким Email уже существует.");
+        }
+
         User newUser = userRepository.create(UserMapper.mapToUser(userDto)).get();
         return UserMapper.mapToUserDto(newUser);
     }
@@ -42,6 +50,15 @@ public class UserServiceImpl implements UserService {
     public UserDto update(UserDto userDto, Long id) {
         User updatedUser = userRepository.getUserById(id).orElseThrow(() ->
                 new UserNotFoundException(String.format("Пользователь %d не найден.", id)));
+
+        final String userEmail = userRepository.getUserById(id).get().getEmail();
+        final String updatedUserEmail = userDto.getEmail();
+
+        Set<String> emails = getAllEmails();
+
+        if (emails.contains(updatedUserEmail) && !userEmail.equals(updatedUserEmail)) {
+            throw new AlreadyExistsException("Пользователь с таким Email уже существует.");
+        }
 
         if (userDto.getName() != null && !userDto.getName().isBlank()) {
             updatedUser.setName(userDto.getName());
@@ -57,5 +74,12 @@ public class UserServiceImpl implements UserService {
     public void deleteUser(Long userId) {
         userRepository.getUserById(userId);
         userRepository.delete(userId);
+    }
+
+    private Set<String> getAllEmails() {
+        return userRepository.getAllUser()
+                .stream()
+                .map(User::getEmail)
+                .collect(Collectors.toSet());
     }
 }
