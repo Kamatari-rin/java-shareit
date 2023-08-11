37b7ae37b5072c4d88da1f97ba2642b384daef08
Kamatari-rin <kamatarisama@gmail.com>
2023-08-11 16:46:45 +0500

    gateWay fix style ErrorHandler

--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -3,7 +3,7 @@ package ru.practicum.shareit.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
